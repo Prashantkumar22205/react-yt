71164d5cbbcc8b2866f33e33e6afbf42853b4250
Prashant Kumar <prashantkumar22205@gmail.com>
2025-10-16 00:27:35 +0530

    component

---
+++ 02-folder/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>PRASHANT KUMAR</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima totam culpa illo voluptatum facere fugit cumque. Voluptatum, numquam? Cum commodi perspiciatis corrupti numquam qui reiciendis. Dolorum deleniti quaerat illum iusto?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
